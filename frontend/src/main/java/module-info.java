a280476fcad7897e83f6a3a0dbe7d6d364106139
Johnny Le <jwonnyleaf@gmail.com>
2022-10-11 00:02:00 -0500

    Database and reformatting

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module com.involuntary.revpos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.involuntary.revpos to javafx.fxml;
     exports com.involuntary.revpos;
     exports com.involuntary.revpos.controller;
     opens com.involuntary.revpos.controller to javafx.fxml;
+    exports com.involuntary.revpos.database;
+    opens com.involuntary.revpos.database to javafx.fxml;
 }
