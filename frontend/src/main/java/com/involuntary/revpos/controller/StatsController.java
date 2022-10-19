c61add86d4c4ebe12008f1503d4e31a706b4e580
Cuchador <willmiranda2002@gmail.com>
2022-10-18 21:24:18 -0500

    added report button and fxml page

--- frontend/src/main/java/com/involuntary/revpos/controller/StatsController.java
+++ frontend/src/main/java/com/involuntary/revpos/controller/StatsController.java
@@ -1,4 +1,20 @@
 package com.involuntary.revpos.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+
 public class StatsController extends ManagerController {
+    @FXML
+    private ImageView manageReportBtn;
+
+    public void openManageReport() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/reports.fxml"));
+        Scene scene = manageReportBtn.getScene();
+        scene.setRoot(root);
+    }
 }
